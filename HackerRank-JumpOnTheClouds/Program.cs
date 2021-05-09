1c7b4a6de95481cf07145f4a154b487d23d66f07
DESKTOP\Yatong <mike.z@anzuk.education>
2021-05-09 15:14:09 +1000

    HackerRank-JumpOnTheClouds

---
+++ HackerRank-JumpOnTheClouds/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Result
+{
+
+    /*
+     * Complete the 'jumpingOnClouds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY c as parameter.
+     */
+
+    public static int jumpingOnClouds(List<int> c)
+    {
+        var step = 0;
+        for (var i = 0; i < c.Count - 1;)
+        {
+            i += i + 2 < c.Count && c[i + 2] == 0 ? 2 : 1;
+            step++;
+        }
+        return step;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> c = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(cTemp => Convert.ToInt32(cTemp)).ToList();
+
+        int result = Result.jumpingOnClouds(c);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
